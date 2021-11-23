f6d4949a7cd61c589300779724e513357ce1a599
yearthmain <yearthmain@gmail.com>
2021-11-23 23:01:23 +0800

    [feat]: v3 - 通过 context 配置默认 fetcher

--- src/hooks/useFetch/index.js
+++ src/hooks/useFetch/index.js
@@ -1,17 +1,21 @@
-import { useState, useEffect } from "react";
-import { prefixUrl } from "../shared";
+import { useState, useEffect, useContext } from "react";
+import { useFetchConfigContext } from "./context";
+import { prefixUrl } from "../../shared";
 
 export const useFetch = (url, fetcher) => {
   const [state, setState] = useState(undefined);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const config = useContext(useFetchConfigContext);
+  const _fetcher = typeof fetcher === "function" ? fetcher : config.fetcher;
+
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
 
       try {
-        const data = await fetcher(`${prefixUrl}${url}`);
+        const data = await _fetcher(`${prefixUrl}${url}`);
         setState(data.result);
       } catch (err) {
         setError(true);
@@ -24,3 +28,6 @@ export const useFetch = (url, fetcher) => {
 
   return [state, error, loading];
 };
+
+const useFetchConfig = useFetchConfigContext.Provider;
+export { useFetchConfig };
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { useFetchConfig as UseFetchConfig } from "./hooks/useFetch";
+import { customFetch } from "./shared";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <UseFetchConfig value={{ fetcher: customFetch }}>
+    <App />
+  </UseFetchConfig>,
+  document.getElementById("root")
+);
